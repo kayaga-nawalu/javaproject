c251b056abf0aaa8c404a0c83e0e6787684f41c2
kayaga <kayaga@nawal.noreply.github.com>
2024-10-04 16:51:13 +0300

    first commit.

---
+++ tanaoopassignment/src/tanaoopassignment/loopassignment.java
@@ -0,0 +1,11 @@
+package tanaoopassignment;
+
+public class loopassignment {
+
+    public static void main(String[] args) {
+        // Print uppercase letters A-Z
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c + " ");
+        }
+    }
+}
